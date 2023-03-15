b9031b538e4099f5f6985e1d2ae968f630b5fd9d
github <filipe.boeck@hotmail.com>
2023-03-15 08:27:08 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by wolf on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
