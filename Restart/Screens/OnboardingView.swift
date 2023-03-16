8074a9c8d1a79df8bc889e224bfaee64fa478485
github <filipe.boeck@hotmail.com>
2023-03-16 19:11:13 -0300

    feat: app finish

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -19,6 +19,8 @@ struct OnboardingView: View {
   @State private var indicatorOpacity: Double = 1.0
   @State private var textTitle: String = "Share."
   
+  let hapticFeedback = UINotificationFeedbackGenerator()
+  
   // MARK: - BODY
   
   var body: some View {
@@ -155,9 +157,12 @@ struct OnboardingView: View {
                 .onEnded { _ in
                   withAnimation(Animation.easeOut(duration: 0.4)) {
                     if buttonOffSet > buttonWidth / 2 {
+                      hapticFeedback.notificationOccurred(.success)
+                      playSound(sound: "chimeup", type: "mp3")
                       buttonOffSet = buttonWidth - 80
                       isOnboardingViewActive = false
                     } else {
+                      hapticFeedback.notificationOccurred(.warning)
                       buttonOffSet = 0
                     }
                   }
@@ -177,6 +182,7 @@ struct OnboardingView: View {
     .onAppear(perform: {
       isAnimating = true
     })
+    .preferredColorScheme(.dark)
   }
 }
 
